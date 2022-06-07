d8af794617782cf5a2a95a2571b32baaeb40a4c4
QuangDuc-HUST <ducquangng.03@gmail.com>
2022-06-07 18:54:11 +0700

    First Demo Creation

---
+++ demo/ForceSimulationDemoProject/src/ForceSimulationApp.java
@@ -0,0 +1,30 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ForceSimulationApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/ForceSimulationApp.fxml") );
+		Parent p = fxmlLoader.load();
+		
+//		BackgroundController c = fxmlLoader.getController();
+		
+		Scene scene = new Scene(p);
+
+		primaryStage.setTitle( "BackgroundApp" );
+		primaryStage.setScene( scene );
+//		primaryStage.setOnShown(event ->  c.startAmination() );
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
