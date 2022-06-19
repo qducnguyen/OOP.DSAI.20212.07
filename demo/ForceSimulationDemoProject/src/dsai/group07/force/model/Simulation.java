b43e33d5c72000960e506beaa16af951ddbd519c
DungNaSa10 <dungnasa10@gmail.com>
2022-06-19 12:21:29 +0700

    Testing code for Simulation

--- demo/ForceSimulationDemoProject/src/dsai/group07/force/model/Simulation.java
+++ demo/ForceSimulationDemoProject/src/dsai/group07/force/model/Simulation.java
@@ -3,7 +3,11 @@ package dsai.group07.force.model;
 import dsai.group07.force.model.object.Cube;
 import dsai.group07.force.model.object.MainObject;
 import dsai.group07.force.model.surface.Surface;
+import dsai.group07.force.model.vector.AppliedForce;
+import dsai.group07.force.model.vector.Force;
+import dsai.group07.force.model.vector.FrictionForce;
 import dsai.group07.force.model.vector.HorizontalVector;
+import dsai.group07.force.model.vector.NetForce;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
@@ -13,27 +17,36 @@ public class Simulation {
 	private BooleanProperty isStart = new SimpleBooleanProperty(false); 
 	private MainObject obj;
 	private Surface sur;
-	private HorizontalVector aForce;
-	private HorizontalVector fForce;
+	private Force aForce;
+	private Force fForce;
 	
 	public Simulation() {
 		this.obj = new Cube();
 		this.sur = new Surface();
-		this.aForce = new HorizontalVector(0);
-		this.fForce = new HorizontalVector(0);
+		this.aForce = new AppliedForce(0);
+		this.fForce = new FrictionForce(0);
 	}
 	
+	public Simulation(MainObject mainObj, Surface sur, AppliedForce aForce) {
+		this.obj = mainObj;
+		this.sur = sur;
+		this.aForce = aForce;
+		this.fForce = new FrictionForce(0, this);
+	}
 	
 	public MainObject getObj() {
 		return obj;
+	}
+	
+	public void setObject(MainObject obj) {
+		this.obj = obj;
 	}
 	
 	public Surface getSur() {
 		return sur;
 	}
 	
-	
-	public HorizontalVector getaForce() {
+	public Force getaForce() {
 		return aForce;
 	}
 
@@ -41,12 +54,16 @@ public class Simulation {
 		this.aForce.setValue(aForce);
 	}
 
-	public HorizontalVector getfForce() {
+	public Force getfForce() {
 		return fForce;
 	}
 
 	public void setfForce(double fForce) {
 		this.fForce.setValue(fForce);
+	}
+	
+	public Force getNetForce() {
+		return new NetForce(0, aForce, fForce);
 	}
 	
 	public BooleanProperty isStartProperty() {
@@ -60,16 +77,32 @@ public class Simulation {
 	public boolean getIsStart() {
 		return this.isStart.get();
 	}
-	// sumForce
-	// Surfaces
 	
 	public void pause() {
-		///
+		setIsStart(false);
+	}
+	
+	public void cont() {
+		setIsStart(true);
 	}
 	
 	public void restart() {
-		isStart.set(false);
+		setIsStart(false);
+		aForce.setValue(0);
+		fForce.setValue(0);
+		setObject(null);
 	}
 	
+	public void getObjAcc() {
+		getObj().updateAcc(getNetForce());
+	}
+	
+	public HorizontalVector getObjVel() {
+		return getObj().getVel();
+	}
+	
+	public void applyForceInTime(Force force, double t) {
+		getObj().applyForceInTime(force, t);
+	}
 	
 }
