d02877ce6fbee4bd192edaa3d4732906f2235fff
DungNaSa10 <dungnasa10@gmail.com>
2022-07-15 14:40:00 +0700

    Add documentation for Cylinder class

--- code/ForceSimulationProject/src/dsai/group07/force/model/object/Cylinder.java
+++ code/ForceSimulationProject/src/dsai/group07/force/model/object/Cylinder.java
@@ -5,23 +5,59 @@ import dsai.group07.force.model.vector.FrictionForce;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
+/**
+ * This class is used to represent the cylinder object. It extends MainObject
+ * class and implements Rotatable interface. It contains further attributes and
+ * methods for cylinder radius, angle, angular acceleration and angular
+ * velocity.
+ *
+ */
 public class Cylinder extends MainObject implements Rotatable {
-
+	/**
+	 * Holds the angle property of this Cylinder
+	 */
 	private DoubleProperty angle = new SimpleDoubleProperty();
+	/**
+	 * Holds the angular acceleration property of this Cylinder
+	 */
 	private DoubleProperty angAcc = new SimpleDoubleProperty();
+	/**
+	 * Holds the angular velocity property of this Cylinder
+	 */
 	private DoubleProperty angVel = new SimpleDoubleProperty();
+	/**
+	 * Holds the radius property of this Cylinder. Default radius = 0.3 *
+	 * {@link Cylinder#MAX_RADIUS}
+	 */
 	private DoubleProperty radius = new SimpleDoubleProperty(MAX_RADIUS * 0.3);
+	// max and min radius of class Cylinder
 	public static final double MAX_RADIUS = 1.0;
 	public static final double MIN_RADIUS = 0.1;
 
+	/**
+	 * Default class constructor
+	 */
 	public Cylinder() throws Exception {
 		super();
 	}
 
+	/**
+	 * Class constructor specifying mass
+	 * 
+	 * @param mass This Cylinder's mass
+	 * @throws Exception Throw exception if mass <= 0
+	 */
 	public Cylinder(double mass) throws Exception {
 		super(mass);
 	}
 
+	/**
+	 * Class constructor specifying mass and size
+	 * 
+	 * @param mass   This Cylinder's mass
+	 * @param radius This Cylinder's radius
+	 * @throws Exception Exception Throw exception if mass <= 0
+	 */
 	public Cylinder(double mass, double radius) throws Exception {
 		this(mass);
 		setRadius(radius);
@@ -43,11 +79,10 @@ public class Cylinder extends MainObject implements Rotatable {
 	}
 
 	@Override
-	public void updateAngAcc(Force force) throws Exception {
+	public void updateAngAcc(Force force) {
+		// In case there is no friction force, cylinder just translates
 		if (force instanceof FrictionForce) {
 			setAngAcc(-force.getValue() / (0.5 * getMass() * getRadius() * getRadius()));
-		} else {
-			throw new Exception("No friction force, object just translates");
 		}
 	}
 
@@ -112,20 +147,20 @@ public class Cylinder extends MainObject implements Rotatable {
 	}
 
 	@Override
-	public void applyForceInTimeRotate(Force force, double t) throws Exception {
+	public void applyForceInTimeRotate(Force force, double t) {
+		// Gets this Cylinder old angular velocity and update
+		// its angular acceleration, angular velocity and angle
 		double oldAngVel = getAngVel();
 		updateAngAcc(force);
 		updateAngVel(t);
 		updateAngle(oldAngVel, t);
 	}
-	
+
 	@Override
 	public void applyForceInTime(Force netforce, Force fForce, double t) {
+		// Applies netForce (sum of forces) for translation
 		super.applyForceInTime(netforce, fForce, t);
-		try {
-			this.applyForceInTimeRotate(fForce, t);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// Applies fForce (friction force) for rotation
+		this.applyForceInTimeRotate(fForce, t);
 	}
 }
